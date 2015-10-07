241e4adde7d6df1e518a9ac8f113ecd99cee3e05
Falkirks <me@falkirks.com>
2015-10-06 22:32:04 -0700

    Automagic <pre> tag support Eventually this could be implemented onto the tree generating parser for optimization.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 'use strict';
 var htmlparser = require("htmlparser2");
+function getRichTextContent(node){
+  var output = "";
+  if(node.type == 'text'){
+    return node.data;
+  }
+  for(var i = 0; i < node.children.length; i++){
+    if(node.children[i].type == 'tag') output += "<" + node.children[i].name + ">";
+    output += getRichTextContent(node.children[i]);
+    if(node.children[i].type == 'tag') output += "</" + node.children[i].name + ">";
+  }
+  return output;
+}
 module.exports = function(html, cb){
   var handler = new htmlparser.DomHandler(function (error, dom) {
     if (error){
@@ -10,6 +22,15 @@ module.exports = function(html, cb){
       var out = "";
       var compile = function(output, nodes){
         for(var i = 0; i < nodes.length; i++){
+          if(nodes[i].type == 'tag' && nodes[i].name == 'pre'){
+              nodes[i] = {
+                'type': 'text',
+                'data': getRichTextContent(nodes[i]),
+                'next': nodes[i].next,
+                'prev': nodes[i].prev,
+                'parent': nodes[i].parent
+              };
+          }
           if(nodes[i].parent != null && nodes[i].parent.type == 'tag'){
             if(nodes[i].parent.name == 'u') {
               if (nodes[i].type == 'text') {
